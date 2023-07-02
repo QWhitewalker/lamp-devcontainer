8c73fff5fde86df6efa9a94279fdb22f8150668d
QWhitewalker <gion.alfanz@hotmail.com>
2023-07-02 08:35:53 +0000

    comment code

--- .devcontainer/db/forest.sql
+++ .devcontainer/db/forest.sql
@@ -1,12 +1,16 @@
+-- creatin a database named "forest"
 CREATE DATABASE forest;
 
+-- opening the database "forest"
 USE forest;
 
+-- creating a new table named "tbl_tree"
 CREATE TABLE tbl_tree(
 PID INT NOT NULL AUTO_INCREMENT,
 name VARCHAR(50),
 PRIMARY KEY(PID)
 );
 
+-- inserting 5 rows into the table "tbl_tree"
 INSERT tbl_tree (name)
 VALUES ('Oak'),('Maple'),('Birch'),('Elm'),('Hawthorn');
--- html/index.php
+++ html/index.php
@@ -9,22 +9,26 @@
 </head>
 
 <body>
+  <!-- PHP Hello World-->
   <?php
   echo "<p>Hello World</p>";
- 
-$servername = "db";
-$username = "root";
-$password = "password";
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=forest", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
   ?>
-   
+
+  <!-- Database Connection test-->
+  <?php
+  $servername = "db";
+  $username = "root";
+  $password = "password";
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=forest", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+  ?>
+
 </body>
 
 </html>
